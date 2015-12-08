90f8c2a9692435acbf4a9d6e7ae76edf71e11dc0
Mike Maraya <mike.maraya@gmail.com>
2015-12-07 20:44:38 -0500

    replace ARG_MAX with POSIX sysconf call, fixes #1

--- ftp/glob.c
+++ ftp/glob.c
@@ -50,6 +50,7 @@ char glob_rcsid[] =
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "ftp_var.h"  /* for protos only */
 #include "glob.h"
@@ -57,6 +58,8 @@ char glob_rcsid[] =
 #define	QUOTE 0200
 #define	TRIM 0177
 #define	eq(a,b)		(strcmp(a, b)==0)
+
+#define	ARG_MAX		(sysconf(_SC_ARG_MAX))
 #define	GAVSIZ		(ARG_MAX/6)
 #define	isdir(d)	((d.st_mode & S_IFMT) == S_IFDIR)
 
